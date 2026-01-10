0d9f10d70d5670771a7aec0fdfb4bc9c49bb3f6f
Aryan <aryangadhave202@gmail.com>
2026-01-11 01:44:30 +0530

    Strength_done

--- src/components/Strength.jsx
+++ src/components/Strength.jsx
@@ -1,17 +1,45 @@
 import React from 'react'
 import Cylinder from './Cylinder'
+import { useMediaQuery } from 'react-responsive'
+
 const Strength = () => {
+
+  const isSmallScreen = useMediaQuery({ query: '(max-width: 1400px)' })
+
   return (
     <div className='Strength'>
-      <div className="first">
+     { !isSmallScreen && <div className="second-lg">
+        <div className="imgCont">
+          <img src="/src/images/4.jpeg" alt="" />
+        </div>
+        <div className="imgCont">
+            <img src="/src/images/2.jpeg" alt="" />
+        </div>
+      </div>}
+
+     { isSmallScreen && <div className="first">
         <h1>My Strength</h1>
         <h3></h3>
-     
-      </div>
-      <div className="second-sm">
-        <Cylinder/>
-      </div>
+
+      </div>}
+
+      {
+        isSmallScreen &&
+        <div className="second-sm">
+          <Cylinder />
+        </div>
+      }
       <div className="third">
+        {
+
+           !isSmallScreen &&
+          <div className="first">
+            <h1>My Strength</h1>
+            <h3></h3>
+
+          </div>}
+             <div className="cards">
+
         <div className="scard">
           <span className='tick'><i class="ri-check-fill"></i></span>
           <p>  <span className='head'>Adaptability:</span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem ex dolore voluptatum modi aliquam mollitia libero praesentium itaque labore numquam.</p>
@@ -28,6 +56,7 @@ const Strength = () => {
           <span className='tick'><i class="ri-check-fill"></i></span>
           <p>  <span className='head'>Tech-Savvy:</span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem ex dolore voluptatum modi aliquam mollitia libero praesentium itaque labore numquam.</p>
         </div>
+             </div>
       </div>
     </div>
   )
